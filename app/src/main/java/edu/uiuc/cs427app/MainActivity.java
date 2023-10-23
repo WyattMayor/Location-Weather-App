2407fda054e2dce909910330178a44f30308776e
WyattMayor <wpmayor@monmouthcollege.edu>
2023-10-23 01:46:06 -0500

    Added dynamic button menu, added City list in database still work in progress

--- app/src/main/java/edu/uiuc/cs427app/MainActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/MainActivity.java
@@ -1,17 +1,17 @@
 package edu.uiuc.cs427app;
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
 import androidx.navigation.ui.AppBarConfiguration;
-
 import edu.uiuc.cs427app.databinding.ActivityMainBinding;
-
 import android.widget.Button;
+import android.widget.LinearLayout;
+import java.util.*;
+import android.database.sqlite.SQLiteDatabase;
+import edu.uiuc.cs427app.db.*;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,44 +22,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        LinearLayout buttonContainer = findViewById(R.id.buttonContainer);
         // Initializing the UI components
         // The list of locations should be customized per user (change the implementation so that
         // buttons are added to layout programmatically
-        Button buttonChampaign = findViewById(R.id.buttonChampaign);
-        Button buttonChicago = findViewById(R.id.buttonChicago);
-        Button buttonLA = findViewById(R.id.buttonLA);
-        Button buttonNew = findViewById(R.id.buttonAddLocation);
 
-        buttonChampaign.setOnClickListener(this);
-        buttonChicago.setOnClickListener(this);
-        buttonLA.setOnClickListener(this);
-        buttonNew.setOnClickListener(this);
 
+        /*Query statements that query based on username
+        String query = "SELECT Item.city FROM Item " +
+                "INNER JOIN User ON Item.userName = User.username " +
+                "WHERE User.username = ?";*/
+
+        //for testing - Wyatt Mayor
+        String[] userLocations = {"Chicago", "Los Angeles", "Champaign", "New York", "New Jersey"};
+        List<String> userLocation = Arrays.asList(userLocations);
+        //List<String> userLocations = getUserLocations();
+        for (String city : userLocation){
+            //Create the button
+            Button button= new Button(this);
+            button.setText(city);
+            button.setId(View.generateViewId());
+            button.setOnClickListener(this);
+
+            //Set background and text color of the button
+            button.setBackgroundColor(getResources().getColor(R.color.purple_500));
+            button.setTextColor(getResources().getColor(R.color.white));
+
+            //Add margins between individual buttons in dynamic list
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+
+            int marginInDp = 8;
+
+            layoutParams.setMargins(0, 0, 0, marginInDp);
+            button.setLayoutParams(layoutParams);
+
+            //create shape to round edges of the button
+            GradientDrawable roundedCorners = new GradientDrawable();
+            roundedCorners.setShape(GradientDrawable.RECTANGLE);
+            roundedCorners.setColor(getResources().getColor(R.color.purple_500));
+            roundedCorners.setCornerRadius(20);
+            button.setBackgroundDrawable(roundedCorners);
+
+            //Add the button to the button container
+            buttonContainer.addView(button);
+
+        }
     }
 
     @Override
     public void onClick(View view) {
         Intent intent;
         switch (view.getId()) {
-            case R.id.buttonChampaign:
-                intent = new Intent(this, DetailsActivity.class);
-                intent.putExtra("city", "Champaign");
-                startActivity(intent);
-                break;
-            case R.id.buttonChicago:
-                intent = new Intent(this, DetailsActivity.class);
-                intent.putExtra("city", "Chicago");
-                startActivity(intent);
-                break;
-            case R.id.buttonLA:
-                intent = new Intent(this, DetailsActivity.class);
-                intent.putExtra("city", "Los Angeles");
-                startActivity(intent);
-                break;
             case R.id.buttonAddLocation:
                 // Implement this action to add a new location to the list of locations
                 break;
+            default:
+                Button clicked = (Button) view;
+                String cityName = clicked.getText().toString();
+
+                intent = new Intent(this, DetailsActivity.class);
+                intent.putExtra("city", cityName);
+                startActivity(intent);
+                break;
+
         }
     }
 }
--- app/src/main/java/edu/uiuc/cs427app/db/DatabaseHelper.java
+++ app/src/main/java/edu/uiuc/cs427app/db/DatabaseHelper.java
@@ -13,6 +13,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             + "username TEXT NOT NULL,"
             + "password TEXT NOT NULL);";
 
+    private static final String CREATE_ITEM_TABLE = "CREATE TABLE Item ("
+            + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + "city TEXT NOT NULL,"
+            + "userName TEXT," // Add a foreign key reference to the User table
+            + "FOREIGN KEY (userName) REFERENCES User(username));"; // Ensure referential integrity
+
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -20,6 +26,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_USER_TABLE);
+        db.execSQL(CREATE_ITEM_TABLE);
     }
 
     @Override
