182858db48ef0af20022340136cf99e819e89657
WyattMayor <wpmayor@monmouthcollege.edu>
2023-11-09 15:10:43 -0600

    Weather api implementation

--- app/src/main/java/edu/uiuc/cs427app/WeatherActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/WeatherActivity.java
@@ -1,6 +1,9 @@
 package edu.uiuc.cs427app;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +16,25 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import static java.lang.System.*;
+import static java.security.AccessController.getContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import edu.uiuc.cs427app.util.LogoutComponent;
 
@@ -45,7 +67,89 @@ public class WeatherActivity extends AppCompatActivity implements View.OnClickLi
         renderCurrentDateTime(cityName);
 
         // Placeholder for rendering weather.
-        RenderWeatherData("70°F", "Sunny", "50%", "5 mph NW");
+        RenderWeatherData( cityName, WeatherActivity.this);
+    }
+    public interface LatLonCallback {
+        void onLatLonReceived(double latitude, double longitude);
+    }
+
+    public interface WeatherCallback {
+        void onWeatherDataReceived(double temperature, double windSpeed, double windDegrees,double humidity, String weatherDescription);
+    }
+    private void getLatLon(String city, Context context, LatLonCallback callback) {
+        String baseUrl = "https://api.openweathermap.org/geo/1.0/direct";
+        String apiKey = "e05da8e2e7eec0f149c076e2650144f5";
+        String url = baseUrl + "?q=" + city + "," + "+1" +
+                "&limit=1" + "&appid=" + apiKey;
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        final double[] latitude = {0};
+        final double[] longitude = {0};
+
+        JsonArrayRequest JsonArrayRequest = new JsonArrayRequest(Request.Method.GET,url,null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            JSONObject firstObject = response.getJSONObject(0);
+                            latitude[0] = firstObject.getDouble("lat");
+                            longitude[0] = firstObject.getDouble("lon");
+                            callback.onLatLonReceived(latitude[0], longitude[0]);
+
+                        }
+                        catch(JSONException e){
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle errors here
+                        System.err.println("Error: " + error.toString());
+                    }
+                });
+        requestQueue.add(JsonArrayRequest);
+    }
+
+    private void getWeatherData(double latitude, double longitude, Context context, WeatherCallback callback) {
+        String baseWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?";
+        String apiKey = "e05da8e2e7eec0f149c076e2650144f5";
+        String urlWeather = baseWeatherUrl + "lat=" + String.valueOf(latitude) + "&lon=" + String.valueOf(longitude) + "&appid=" + apiKey;
+        final double[] temp = {0};
+        final double[] wind_speed = {0};
+        final double[] wind_deg = {0};
+        final double[] humidity2 = {0};
+        final String[] weather2 = {""};
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        Log.d("check2", urlWeather);
+        JsonObjectRequest jsonArrayRequestWeather = new JsonObjectRequest(Request.Method.GET, urlWeather, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+                            JSONObject main = response.getJSONObject("main");
+                            JSONObject wind = response.getJSONObject("wind");
+                            JSONArray weatherparse = response.getJSONArray("weather");
+                            JSONObject weatherparser2 = weatherparse.getJSONObject(0);
+                            temp[0] = main.getDouble("temp");
+                            wind_speed[0] = wind.getDouble("speed");
+                            wind_deg[0] = wind.getDouble("deg");
+                            humidity2[0] = main.getDouble("humidity");
+                            weather2[0] = weatherparser2.getString("main");
+                            callback.onWeatherDataReceived(temp[0],  wind_speed[0], wind_deg[0],  humidity2[0], weather2[0]);
+                        }
+                        catch(JSONException e){
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle errors here
+                        System.err.println("Error: " + error.toString());
+                    }
+                });
+        requestQueue.add(jsonArrayRequestWeather);
     }
 
     /**
@@ -53,23 +157,51 @@ public class WeatherActivity extends AppCompatActivity implements View.OnClickLi
      * This method serves as a placeholder for rendering weather data on the UI.
      * It should be connected to a weather API to fetch and display live data.
      *
-     * @param temperature    The temperature to display.
-     * @param weather        The weather condition (sunny, partly sunny, cloudy, partly cloudy, foggy, etc.) to display.
-     * @param humidity       The humidity to display.
-     * @param windCondition  The wind condition to display.
      */
-    private void RenderWeatherData(String temperature, String weather, String humidity, String windCondition) {
-        TextView temperatureTextView = findViewById(R.id.temperatureText);
-        temperatureTextView.setText(temperature);
+    private void RenderWeatherData(String City, Context context) {
+
+        double temperature = 0.0;
+        String weatherDescription = "";
+        double humidity= 0.0;
+        double windSpeed= 0.0;
+        double windDegrees= 0.0;
+
+        getLatLon(City, this, new LatLonCallback() {
+            @Override
+            public void onLatLonReceived(double latitude, double longitude) {
+                // First request completed, now make the second request
+                getWeatherData(latitude, longitude, WeatherActivity.this, new WeatherCallback() {
+                    @Override
+                    public void onWeatherDataReceived(double temperature, double windSpeed, double windDegrees, double humidity, String weatherDescription) {
+                        // Handle weather data here
+                        Log.d("Weather", "Temperature: " + temperature + ", Wind Speed: " + windSpeed + ", Weather: " + weatherDescription);
+                        TextView temperatureTextView = findViewById(R.id.temperatureText);
+                        temperatureTextView.setText(String.valueOf(temperature));
+
+                        TextView weatherConditionTextView = findViewById(R.id.weatherConditionText);
+                        weatherConditionTextView.setText(weatherDescription);
+
+                        TextView humidityTextView = findViewById(R.id.humidityText);
+                        humidityTextView.setText("Humidity: " + String.valueOf(humidity));
+
+                        TextView windConditionTextView = findViewById(R.id.windConditionText);
+                        windConditionTextView.setText("Wind Speed: " + String.valueOf(windSpeed)+  " Wind deg: " + String.valueOf(windDegrees));
+                    }
+                });
+            }
+        });
+
+        /*TextView temperatureTextView = findViewById(R.id.temperatureText);
+        temperatureTextView.setText(String.valueOf(temperature));
 
         TextView weatherConditionTextView = findViewById(R.id.weatherConditionText);
-        weatherConditionTextView.setText(weather);
+        weatherConditionTextView.setText(weatherDescription);
 
         TextView humidityTextView = findViewById(R.id.humidityText);
-        humidityTextView.setText("Humidity: " + humidity);
+        humidityTextView.setText("Humidity: " + String.valueOf(humidity));
 
         TextView windConditionTextView = findViewById(R.id.windConditionText);
-        windConditionTextView.setText("Wind: " + windCondition);
+        windConditionTextView.setText("Wind: " + String.valueOf(windSpeed)+  "Wind deg: " + String.valueOf(windDegrees));*/
     }
 
     /**
@@ -81,6 +213,8 @@ public class WeatherActivity extends AppCompatActivity implements View.OnClickLi
      * @param city The name of the city for which to display the current date and time.
      */
     private void renderCurrentDateTime(String city) {
+
+
         // Map the city to its time zone
         String timeZoneId = mapCityToTimeZone(city);
 
