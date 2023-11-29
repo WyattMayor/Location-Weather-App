02aba08bbcd90aa8c0ad11ab2d3ed6fb37c24343
WyattMayor <wmayor@monmouthcollege.edu>
2023-11-28 21:31:11 -0600

    testing code

--- app/src/androidTest/java/edu/uiuc/cs427app/RemoveLocActivityTest.java
+++ app/src/androidTest/java/edu/uiuc/cs427app/RemoveLocActivityTest.java
@@ -6,9 +6,28 @@ import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.matcher.IntentMatchers;
 import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.espresso.ViewAssertion;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.espresso.Espresso;
+import androidx.test.filters.LargeTest;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.core.AllOf.allOf;
+
+import android.view.View;
+
+import org.hamcrest.Matcher;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,46 +35,92 @@ import org.mockito.Mockito;
 
 import edu.uiuc.cs427app.db.DatabaseHelper;
 
+@LargeTest
 @RunWith(AndroidJUnit4.class)
 public class RemoveLocActivityTest {
     @Before
-    public void launchActivity() {
-        ActivityScenario.launch(RemoveLocActivity.class);
+    public void LaunchLoginActivity() {
+        ActivityScenario.launch(LoginActivity.class);
     }
+
+    @Rule
+    public ActivityScenarioRule<LoginActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(LoginActivity.class);
     @Test
-    public void testCityRemoval() {
+    public void testCityAddingAndRemoval() {
+        String username = "wpmayor2";
+        String password = "123";
 
-        DatabaseHelper mockDatabase = Mockito.mock(DatabaseHelper.class);
+        /*signup*/
 
+        onView(withId(R.id.signupLink)).perform(click());
 
+        onView(withId(R.id.usernameEditText))
+                .perform(ViewActions.typeText(username), ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.usernameEditText))
+                .check(ViewAssertions.matches(ViewMatchers.withText(username)));
+
+        onView(withId(R.id.passwordEditText))
+                .perform(ViewActions.typeText(password), ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.passwordEditText))
+                .check(ViewAssertions.matches(ViewMatchers.withText(password)));
+
+        onView(withId(R.id.signupButton))
+                .perform(click());
+
+        /*login*/
+
+        onView(withId(R.id.signupLink)).perform(click());
+
+        onView(withId(R.id.usernameEditText))
+                .perform(ViewActions.typeText(username), ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.usernameEditText))
+                .check(ViewAssertions.matches(ViewMatchers.withText(username)));
+
+        onView(withId(R.id.passwordEditText))
+                .perform(ViewActions.typeText(password), ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.passwordEditText))
+                .check(ViewAssertions.matches(ViewMatchers.withText(password)));
+
+        onView(withId(R.id.loginButton))
+                .perform(click());
+
+        /*add city*/
         String testLocation = "Champaign";
 
-        //TODO create mock database
+        onView(withId(R.id.buttonAddLocation)).perform(click());
 
-        //TODO Add user
-
-        //TODO Add Champaign
-
-        //TODO Assert Champaign is in the database
-
-        // adds city name to edit text box
-        Espresso.onView(ViewMatchers.withId(R.id.locationEditText))
+        onView(withId(R.id.locationEditText))
                 .perform(ViewActions.typeText(testLocation), ViewActions.closeSoftKeyboard());
 
-        // asserts that edit text box has champaign in it
-        Espresso.onView(ViewMatchers.withId(R.id.locationEditText))
+        onView(withId(R.id.locationEditText))
                 .check(ViewAssertions.matches(ViewMatchers.withText(testLocation)));
 
-        //clicks remove location button
-        Espresso.onView(ViewMatchers.withId(R.id.RemoveLocationB))
-                .perform(ViewActions.click());
+        onView(withId(R.id.AddLocationB))
+                .perform(click());
 
-        // asserts that main activity is started after the remove button is clicked
-        Intents.intended(IntentMatchers.hasComponent(MainActivity.class.getName()));
-    }
-    @Test
-    public void CheckCityRemoval() {
-        //TODO Assert Champaign is not in the database
+        /*check that the city is in list*/
+        String expectedCity = "CHAMPAIGN"; // Replace with the expected button text
+        Matcher<View> buttonMatcher = allOf(withText(expectedCity), isDescendantOfA(withId(R.id.buttonContainer)));
+
+        onView(buttonMatcher).check(matches(isDisplayed()));
+
+
+        /*remove city*/
+        onView(withId(R.id.buttonRemoveLocation)).perform(click());
+
+        onView(withId(R.id.locationEditText))
+                .perform(ViewActions.typeText(testLocation), ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.locationEditText))
+                .check(ViewAssertions.matches(ViewMatchers.withText(testLocation)));
+
+        onView(withId(R.id.RemoveLocationB))
+                .perform(click());
     }
 
 }
